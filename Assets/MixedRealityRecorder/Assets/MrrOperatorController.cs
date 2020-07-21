f501f93f8eb85388f4c0576313bc0a45a0c268ab
Eric Jansen <eric.jansen@study.hs-duesseldorf.de>
2020-07-21 17:01:08 +0200

    added basic interaction for operator controller

---
+++ Assets/MixedRealityRecorder/Assets/MrrOperatorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace MRR.Controller
+{
+
+    public class MrrOperatorController : MonoBehaviour
+    {
+        // a reference to the action
+        public SteamVR_Action_Boolean OperatorPoint;
+        // a reference to the hand
+        private SteamVR_Input_Sources handType = SteamVR_Input_Sources.LeftHand;
+        //reference to the sphere
+        public GameObject HighlightPrefab;
+
+        void Start()
+        {
+            OperatorPoint.AddOnStateDownListener(TriggerDown, handType);
+            OperatorPoint.AddOnStateUpListener(TriggerUp, handType);
+        }
+
+        public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+        {
+            Debug.Log("Operator Trigger is up!");
+            HighlightPrefab.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+        {
+            Debug.Log("Operator Trigger is down!");
+            HighlightPrefab.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
