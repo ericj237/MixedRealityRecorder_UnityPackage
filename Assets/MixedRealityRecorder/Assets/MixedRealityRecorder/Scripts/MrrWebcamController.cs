b9d8107d2043f8f407868fbd878dcb79a771abb7
Eric Jansen <eric.jansen@study.hs-duesseldorf.de>
2020-07-22 14:41:15 +0200

    added 3d microphone audio to webcam

--- Assets/MixedRealityRecorder/Assets/MixedRealityRecorder/Scripts/MrrWebcamController.cs
+++ Assets/MixedRealityRecorder/Assets/MixedRealityRecorder/Scripts/MrrWebcamController.cs
@@ -9,11 +9,25 @@ namespace MRR.Controller
 
         private WebCamTexture webCamTexture;
 
+        private void StartAudio()
+        {
+            AudioSource audioSource = this.gameObject.GetComponent<AudioSource>();
+            audioSource.clip = Microphone.Start("Microphone (Realtek(R) Audio)", true, 10, 44100);
+            audioSource.loop = true;
+            //while (!(Microphone.GetPosition(null) > 0)) { }
+            audioSource.Play();
+            //foreach (var device in Microphone.devices)
+            //{
+            //    Debug.Log("Name: " + device);
+            //}
+        }
+
         public void StartWebcam(string deviceName)
         {
             webCamTexture = new WebCamTexture(deviceName);
             webCamTexture.Play();
             this.gameObject.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+            StartAudio();
         }
     }
 }
